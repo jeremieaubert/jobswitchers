80e9479215787e399971747e3708d265c02c30fa
Jeremie Aubert <jeremie.aubert@tuta.io>
2019-08-21 18:50:31 +0200

    new and create

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,15 @@
 class Question < ApplicationRecord
   has_one :answer
+
+  def is_tech?
+    self.tech_a == "1"
+  end
+
+  def is_craft?
+    self.craft_a == "1"
+  end
+
+  def is_social?
+    self.social_a == "1"
+  end
 end
