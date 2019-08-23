46c4e4612328ea1b623802441900d7726121cd5f
Theo Gainnier <theogainnier@gmail.com>
2019-08-23 17:42:10 +0200

    strap finished

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -6,7 +6,6 @@ class TestsController < ApplicationController
   end
 
   def create
-
     tech_a = 0
     social_a = 0
     craft_a = 0
@@ -19,28 +18,23 @@ class TestsController < ApplicationController
       if response == "1"
         if q.is_tech?
           tech_a += 1
-      Answer.create(user: current_user, question: q, response: "1")
+          Answer.create(user: current_user, question: q, response: "1")
         elsif q.is_social?
           social_a += 1
 
-      Answer.create(user: current_user, question: q, response: "1")
+          Answer.create(user: current_user, question: q, response: "1")
         elsif q.is_craft?
           craft_a += 1
 
-      Answer.create(user: current_user, question: q, response: "1")
+          Answer.create(user: current_user, question: q, response: "1")
         end
       end
     end
 
-    value = {type: :tech, val: tech_a}
-    if social_a > value[:val]
-      value = {type: :social, val: social_a}
-    end
-    if craft_a > value[:val]
-      value = {type: :craft, val: craft_a}
-    end
+    value = { type: :tech, val: tech_a }
+    value = { type: :social, val: social_a } if social_a > value[:val]
+    value = { type: :craft, val: craft_a } if craft_a > value[:val]
 
     Test.create(best_score: value[:val], domain: value[:type].to_s)
   end
 end
-
--- app/models/program.rb
+++ app/models/program.rb
@@ -1,3 +1,4 @@
 class Program < ApplicationRecord
   belongs_to :job
+  monetize :price_cents
 end
