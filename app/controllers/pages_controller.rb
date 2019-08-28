acd4ae95172b855b7511ec92a9b61ccbcabd45a4
harini srinivasan <harinsrini@gmail.com>
2019-08-28 10:18:44 +0200

    changs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def programs
     @test = Test.find(params[:format])
+    @program = Program.create(user: current_user, test: @test, price_cents: 200)
   end
 
   def home
