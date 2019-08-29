b12cf32ab9d93db65a841c3b19f7da3be7dff712
Theo Gainnier <theogainnier@gmail.com>
2019-08-29 17:59:33 +0200

    payment stripe on program

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def programs
     @test = Test.find(params[:format])
-    @program = Program.create(user: current_user, test: @test, price_cents: 200)
+    @program = Program.find(params[:format])
+    @order = Order.find(params[:format])
   end
 
   def home
